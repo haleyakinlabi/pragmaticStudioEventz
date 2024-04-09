29ed3bac812c1ec4fdebe23252525bc74cb7c4a9
Haley Akinlabi (haleyakinlabi) <haley.akinlabi@clickfunnels.com>
2024-04-09 13:43:56 -0600

    Editing Records Part One

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "events#index"
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event"
+  get "event/:id/edit" => "events#edit", as: "edit_event"
 end
